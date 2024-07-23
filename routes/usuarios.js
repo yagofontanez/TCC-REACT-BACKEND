4c96e50aa70fc4b6045e16bd7092a95b03b3eda2
yagofontanez <yagofontanez20@icloud.com>
2024-07-22 21:10:17 -0300

    .

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,13 +6,13 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/login', UsuarioController.loginAluno);
 router.get('/:id', UsuarioController.show);
+router.put('/:id', UsuarioController.update);
 
 router.use(authMiddleware);
 
 
 router.get('/', UsuarioController.index);
 router.post('/', UsuarioController.create);
-router.put('/:id', UsuarioController.update);
 router.delete('/:id', UsuarioController.delete);
 
 module.exports = router;
